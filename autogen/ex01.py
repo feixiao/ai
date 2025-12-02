d7a967f1b68ca2146d160775fb8b3b98a52ceeb2
feixiao <feixiao2020@sina.com>
2025-12-02 16:49:40 +0800

    update

--- autogen/ex01.py
+++ autogen/ex01.py
@@ -9,13 +9,22 @@ import os
 import asyncio
 from autogen_core.models import UserMessage
 from autogen_ext.models.ollama import OllamaChatCompletionClient
+import logging
+
+# AutoGen uses standard Python logging module to log events like model calls and responses
+from autogen_core import EVENT_LOGGER_NAME
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(EVENT_LOGGER_NAME)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
 
 async def main():
 	model_name = os.getenv("LLM_MODEL", "deepseek-r1:8b")
 	ollama_model_client = OllamaChatCompletionClient(model=model_name)
 
 	response = await ollama_model_client.create([UserMessage(content="What is the capital of France?", source="user")])
-	print(response)
+	logging.info(response)
 	await ollama_model_client.close()
 
 if __name__ == "__main__":
