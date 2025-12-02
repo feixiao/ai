d1587808101bbd859980205c7423a92352510194
feixiao <feixiao2020@sina.com>
2025-12-02 17:18:54 +0800

    init

--- autogen/ReadMe.md
+++ autogen/ReadMe.md
@@ -1,7 +1,7 @@
 ### autogen 快速入门
 
 
-##### An web-based UI for prototyping with agents without writing code
+#### An web-based UI for prototyping with agents without writing code
 ```shell
 pip install -U autogenstudio
 autogenstudio ui --port 8080 --appdir ./myapp
@@ -9,12 +9,12 @@ autogenstudio ui --port 8080 --appdir ./myapp
 + [AutoGen Studio](https://microsoft.github.io/autogen/stable//user-guide/autogenstudio-user-guide/index.html) 
 
 
-##### AgentChat
+#### AgentChat
 + [《agentchat-user-guide》](https://microsoft.github.io/autogen/stable//user-guide/agentchat-user-guide/quickstart.html)
 + [《A step-by-step guide to using AgentCha》](https://microsoft.github.io/autogen/stable//user-guide/agentchat-user-guide/tutorial/index.html)
 
 
-#####  An event-driven programming framework for building scalable multi-agent AI systems.
+####  An event-driven programming framework for building scalable multi-agent AI systems.
 + [Core](https://microsoft.github.io/autogen/stable//user-guide/core-user-guide/quickstart.html)
 
 
---
+++ autogen/ex02.py
@@ -0,0 +1,42 @@
+
+
+# Agents
+# https://microsoft.github.io/autogen/stable//user-guide/agentchat-user-guide/tutorial/agents.html
+
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import StructuredMessage
+from autogen_agentchat.ui import Console
+# from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.models.ollama import OllamaChatCompletionClient
+import os
+import asyncio
+
+# Define a tool that searches the web for information.
+# For simplicity, we will use a mock function here that returns a static string.
+async def web_search(query: str) -> str:
+    """Find information on the web"""
+    return "AutoGen is a programming framework for building multi-agent applications."
+
+async def main():
+    # Create an agent that uses the OpenAI GPT-4o model.
+    # model_client = OpenAIChatCompletionClient(
+    #     model="gpt-4.1-nano",
+    #     # api_key="YOUR_API_KEY",   
+    # )
+    # ollama cp MFDoom/deepseek-r1-tool-calling:8b deepseek-r1-tool-calling:8b
+    model_name = os.getenv("LLM_MODEL", "deepseek-r1-tool-calling:8b")
+    model_client = OllamaChatCompletionClient(model=model_name)
+          
+    agent = AssistantAgent(
+        name="assistant",
+        model_client=model_client,
+        tools=[web_search],
+        system_message="Use tools to solve tasks.", 
+    )
+
+    # Use asyncio.run(agent.run(...)) when running in a script.
+    result = await agent.run(task="Find information on AutoGen")
+    print(result.messages)
+
+if __name__ == "__main__":
+	asyncio.run(main())
