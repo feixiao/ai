600f442213f766e05dc2705a796e7d1da9187a3d
feixiao <feixiao2020@sina.com>
2025-12-05 13:17:51 +0800

    update

--- autogen/ex03.py
+++ autogen/ex03.py
@@ -11,6 +11,15 @@ from autogen_agentchat.ui import Console
 from autogen_core import CancellationToken
 from autogen_ext.models.ollama import OllamaChatCompletionClient
 
+# RoundRobinGroupChat(轮询群聊)
+# 策略：​ 这是最简单、最直接的策略。它严格按照成员列表 (agents参数) 中智能体的顺序来选择下一个发言者。
+
+# SelectorGroupChat(选择器群聊)
+# 策略：​ 引入了发言者选择器 (speaker_selection_method)​ 的概念。
+# 这个选择器是一个函数，它接收当前的群聊状态（包括消息历史、候选智能体列表、最后发言者等）作为输入，并返回应该被选择的下一个发言者。
+
+# MagenticOneGroupChat(磁性一号群聊)
+# 策略：​ 这是 SelectorGroupChat的一个特定实现。它提供了一个预设的、基于 LLM 的​ speaker_selection_method函数。
 async def main():
 	# Create an OpenAI model client.
     model_name = "qwen3:8b"
